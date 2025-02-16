7de1479708252496259c4967e47e41bdc0f37516
Victoria Leshchenko <victorialeshchenko7@gmail.com>
2025-02-16 21:10:41 +0200

    Added the necessary libraries and created two functions: getRandomNumber and fillingMatrix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <cstdlib>
+#include <ctime>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#define N 100
+#define MIN 1
+#define MAX 10
+
+int matrix[N][N];
+
+int getRandomNumber()
+{
+    return MIN + (rand() % (MAX - MIN + 1));
 }
+
+void fillingMatrix() {
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            matrix[i][j] = getRandomNumber();
+        }
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+
+}
+
+
