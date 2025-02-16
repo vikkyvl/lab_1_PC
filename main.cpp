b9cc772b2bc2f10818640b84c0ac245be94d7a0c
Victoria Leshchenko <victorialeshchenko7@gmail.com>
2025-02-16 21:57:19 +0200

    Created a function to calculate the product in a column and executed by a sequential algorithm

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <cstdlib>
 #include <ctime>
 
-#define N 100
+#define N 10
 #define MIN 1
 #define MAX 10
 
@@ -25,10 +25,50 @@ void fillingMatrix() {
     }
 }
 
-int main()
+void calculatingProductOfColumn(int col)
 {
+    int product = 1;
+
+    for(int row = 0; row < N; row++)
+    {
+        product *= matrix[row][col];
+    }
+
+    matrix[N - 1 - col][col] = product;
+}
+
+void sequentialCalculation()
+{
+    for(int col = 0; col < N; col++)
+    {
+        calculatingProductOfColumn(col);
+    }
+}
+
+/*void printMatrix()
+{
+    for(int row = 0; row < N; row++)
+    {
+        for(int col = 0; col < N; col++)
+        {
+            std::cout << std::setw(10) << matrix[row][col] << " ";
+        }
+        std::cout << std::endl;
+    }
+}*/
+
+int main() {
     srand(time(NULL));
 
+    //std::cout << "Initial Matrix" << std::endl;
+    fillingMatrix();
+    //printMatrix();
+
+    //std::cout << "New Matrix" << std::endl;
+    sequentialCalculation();
+    //printMatrix();
+
+    return 0;
 }
 
 
